b6207e4d811dd02e580ce06d1c9da2496cc7ffdc
Donald Biggers <DuGsEmail@yahoo.com>
2021-11-07 15:59:38 -0600

    added quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var questions = [{
+    question: "Commonly used data types DO Not Include",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts"
+    },
+    {
+    question: "The condition in an if / else statement is enclosed with ______",
+    choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+    answer: "3. parenthesis"
+    },  
+    {
+    question: "Arrays in JavaScript can be used to store _____",
+    choices: ["1. numbers", "2. other arrays", "3. booleans", "4. all of the above"],
+    answer: "4. all of the above"
+    },  
+    {
+    question: "String values must be enclosed within _____ when being assigned to variables",
+    choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    answer: "3. quotes"
+    },   
+    {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+    answer: "4. console.log"
+}]
